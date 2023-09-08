e4f5e97a27d233c08f7448e8faf7115b6acce26c
raspiotr <raspiotr@gmail.com>
2023-09-08 23:05:13 +0200

    tast 01 start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryElement(item) {
+  const imgElement = document.createElement('img');
+  imgElement.classList.add('gallery__image');
+  imgElement.setAttribute('src', item.preview);
+  imgElement.setAttribute('alt', item.description);
+  const linkElement = document.createElement('a');
+  linkElement.prepend(imgElement);
+  linkElement.classList.add('gallery__item');
+  linkElement.setAttribute('href', item.original);
+  const listElement = document.createElement('li');
+  listElement.prepend(linkElement);
+  return listElement;
+}
+
+const galleryMarkup = galleryItems.map(picture =>
+  createGalleryElement(picture)
+);
+
+const gallery = document.querySelector('.gallery');
+gallery.prepend(...galleryMarkup);
+
+const lightboxGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
