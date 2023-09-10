5484b8b17804e045f292a4bd2fdf9b572f8907f0
raspiotr <raspiotr@gmail.com>
2023-09-10 22:30:36 +0200

    task 02 ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const saveTimeToLocalStorage = throttle(data => {
+  const currentTime = data.seconds;
+  //console.log(currentTime);
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}, 1000);
+
+player.on('timeupdate', saveTimeToLocalStorage);
+
+function setTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    //console.log(savedTime);
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', setTime);
