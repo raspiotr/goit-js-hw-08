1e6bc3fc640d0f1ec2900a0db658b40434fad38f
raspiotr <raspiotr@gmail.com>
2023-09-11 15:21:52 +0200

    task 03 ready

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+function loadContentFromLocalStorage() {
+  const initialState = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ) || {
+    email: '',
+    message: '',
+  };
+  emailInput.value = initialState.email;
+  messageInput.value = initialState.message;
+}
+
+document.addEventListener('DOMContentLoaded', loadContentFromLocalStorage);
+
+const saveDataToLocalStorage = throttle(() => {
+  const actualState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(actualState));
+}, 500);
+
+emailInput.addEventListener('input', saveDataToLocalStorage);
+messageInput.addEventListener('input', saveDataToLocalStorage);
+
+function submitData(event) {
+  event.preventDefault();
+  const formDataToSend = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  console.log(formDataToSend);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+form.addEventListener('submit', submitData);
